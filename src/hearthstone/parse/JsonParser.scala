136bf13f12f2e8447b9b29fba2330d290fa028f6
Steve <Steve@Mean_Machine>
2016-10-20 01:45:10 -0400

    I'M A REAL UNIT TEST

--- src/hearthstone/parse/JsonParser.scala
+++ src/hearthstone/parse/JsonParser.scala
@@ -1,10 +1,14 @@
 package hearthstone.parse
 
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 class JsonParser {
 	
-	def cardParse(card:String):String ={
-		"BRM_013"
+	
+	//ToDo: fix Big Decimal to Int to go FAST
+	def cardParse(card:String):(String, String, Int) ={
+		val jCard: JsValue = Json.parse(card)
+		((jCard\"id").as[JsString].value,(jCard\"name").as[JsString].value, (jCard\"mana").as[JsNumber].value.toIntExact)
 	}
 }
--- src/hearthstone/parse/JsonParserTest.scala
+++ src/hearthstone/parse/JsonParserTest.scala
@@ -8,21 +8,23 @@ class JsonParserTest extends FunSpec with BeforeAndAfter{
 	
 	var objectUnderTest: JsonParser = new JsonParser
 	
-	before{
-		objectUnderTest = new JsonParser
-	}
 	
-	def assertString(expected: String, actual: String){
-		assert(expected.equals(actual), "Failure. Expected: "+ expected +" Actual: "+ actual)
+	def assertString(expected: String, actual: String, paramName:String){
+		assert(expected.equals(actual), "Failure mathing" + paramName +". Expected: "+ expected +" Actual: "+ actual)
+	}
+	def assertInt(expected: Int, actual: Int, paramName:String){
+		assert(expected == actual, "Failure mathing" + paramName +". Expected: "+ expected +" Actual: "+ actual)
 	}
 	
 	private final val ANY_VALID_CARD:String = "{\"id\":\"BRM_013\",\"name\":\"Quick Shot\",\"mana\":2}"
 	
 	describe("cardParse"){
 		describe("when called on any card"){
-			val parsedCard: String= objectUnderTest.cardParse(ANY_VALID_CARD)
+			val parsedCard= objectUnderTest.cardParse(ANY_VALID_CARD)
 			it("returns the correct id"){
-				assertString("BRM_013", parsedCard)
+				assertString("BRM_013", parsedCard._1,"ID")
+				assertString("Quick Shot", parsedCard._2,"NAME")
+				assertInt(2, parsedCard._3, "MANA")
 			}
 		}
 	}
